1d881306bcbe1e409e7a9bd4b9d7d362d1551360
AndyIsFine160 <159632774+AndyIsFine160@users.noreply.github.com>
2026-02-21 00:28:10 +0800

    Revert "Created Scene2 and made scripts"
    This reverts commit 60f4b4aef2d9ef97857df571769805cf6fa4ae90.

--- Scripts/clockRotate.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var base_degrees_per_second: float = 6.0   # Normal speed (like real seconds)
-@export var time_scale: float = 1.0                # Speed multiplier
-
-func _process(delta):
-	var final_speed = base_degrees_per_second * time_scale
-	rotate_z(deg_to_rad(final_speed * delta))
